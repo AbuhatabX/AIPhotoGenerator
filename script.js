9ad1f4c0ee58ff066463795856901b74e42173a0
AbuhatabX <143858116+AbuhatabX@users.noreply.github.com>
2023-11-21 01:12:13 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const api = "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"
+const api = "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"
 const inp = document.getElementById("inp")
 const images = document.querySelector(".images")
 
